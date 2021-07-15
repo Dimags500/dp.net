74bbe68d0f5acbac4662d33e2f641c5929508a28
John Doe <johndoe@example.com>
2021-07-15 19:48:48 +0300

    asd

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DP.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!2");
+        }
+    }
+}
